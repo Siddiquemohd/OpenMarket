c651266fcf3866ebe12671dfa4bd6e4f242070da
Aamir Dalvi <aamirarifmdalvi@gmail.com>
2026-06-10 18:29:58 +0530

    Swap rendering order of dotted pattern to place it behind the circle background

--- src/app/why-openmarket/page.tsx
+++ src/app/why-openmarket/page.tsx
@@ -168,11 +168,11 @@ export default function WhyOpenMarket() {
                 
                 {/* Portrait Area Container */}
                 <div className="relative h-[328px] w-full overflow-hidden flex justify-center">
+                  {/* Dotted Grid Pattern SVG behind circle and portrait */}
+                  <div className="absolute top-16 right-6 w-52 h-52 opacity-25 pointer-events-none" style={{ backgroundImage: 'radial-gradient(#0FA958 1.5px, transparent 1.5px)', backgroundSize: '10px 10px' }} />
+
                   {/* Pale Green Semi-Circle (Arch) Background behind Portrait */}
                   <div className="absolute left-1/2 bottom-0 h-[210px] w-[420px] -translate-x-1/2 rounded-t-full bg-brand-light-green pointer-events-none" />
-
-                  {/* Dotted Grid Pattern SVG behind portrait but on top of arch */}
-                  <div className="absolute top-16 right-6 w-52 h-52 opacity-25 pointer-events-none" style={{ backgroundImage: 'radial-gradient(#0FA958 1.5px, transparent 1.5px)', backgroundSize: '10px 10px' }} />
 
                   {/* Profile Image - Normal Portrait with bottom crop */}
                   <div className="absolute z-10 bottom-0 w-[400px] h-[328px] overflow-hidden left-1/2 -translate-x-1/2">
